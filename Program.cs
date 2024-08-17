eb26c909196b6f0469038d1955fdc0ff84056acf
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-17 20:13:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskJunior2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageFromTheUser;
+            int numberOfRepetitions;
+
+            Console.WriteLine("Добро пожаловать в 'МИР ПОВТОРЯЮЩИХСЯ СООБЩЕНИЙ'.\n");
+            Console.Write("Введите сообщение : ");
+            messageFromTheUser = Console.ReadLine();
+            Console.Write("Введите кол-во повторений : ");
+            numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRepetitions; i++)
+            {
+                Console.WriteLine(messageFromTheUser);
+            }
+        }
+    }
+}
